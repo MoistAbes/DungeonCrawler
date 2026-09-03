3fa2e07f845bf981327807ff3d891ef1d6021c24
MoistAbes <sebaszymek@gmail.com>
2026-09-04 01:01:45 +0200

    hp bar ui refactoring

--- Source/MyProject/UI/StatBarWidget.cpp
+++ Source/MyProject/UI/StatBarWidget.cpp
@@ -1,12 +1,57 @@
 ﻿#include "StatBarWidget.h"
 #include "Components/ProgressBar.h"
 
+UStatBarWidget::UStatBarWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UStatBarWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	// Jeśli mamy pasek Ghost i nie dogonił jeszcze głównego paska
+	if (GhostProgressBar && !FMath::IsNearlyEqual(CurrentGhostPercent, TargetPercent, 0.001f))
+	{
+		// Jeśli leczenie (wartość wzrosła) -> snapujemy natychmiast
+		if (CurrentGhostPercent < TargetPercent)
+		{
+			CurrentGhostPercent = TargetPercent;
+		}
+		else
+		{
+			// Jeśli obrażenia (wartość spadła) -> płynna interpolacja w dół
+			CurrentGhostPercent = FMath::FInterpTo(CurrentGhostPercent, TargetPercent, InDeltaTime, GhostInterpSpeed);
+		}
+
+		GhostProgressBar->SetPercent(CurrentGhostPercent);
+	}
+}
+
 void UStatBarWidget::UpdateRatio(float CurrentValue, float MaxValue)
 {
-	if (StatProgressBar && MaxValue > 0.0f)
+	if (MaxValue <= 0.0f)
 	{
-		const float Ratio = FMath::Clamp(CurrentValue / MaxValue, 0.0f, 1.0f);
-		StatProgressBar->SetPercent(Ratio);
+		return;
+	}
+
+	TargetPercent = FMath::Clamp(CurrentValue / MaxValue, 0.0f, 1.0f);
+
+	if (StatProgressBar)
+	{
+		StatProgressBar->SetPercent(TargetPercent);
+	}
+
+	// Jeśli pasek Ghost nie istnieje w widżecie lub to pierwsza inicjalizacja
+	if (!GhostProgressBar)
+	{
+		CurrentGhostPercent = TargetPercent;
+	}
+	else if (CurrentGhostPercent < TargetPercent)
+	{
+		// Przy leczeniu pasek Ghost natychmiast rośnie razem z głównym
+		CurrentGhostPercent = TargetPercent;
+		GhostProgressBar->SetPercent(CurrentGhostPercent);
 	}
 }
 
--- Source/MyProject/UI/StatBarWidget.h
+++ Source/MyProject/UI/StatBarWidget.h
@@ -6,12 +6,17 @@
 
 class UProgressBar;
 
+/**
+ * Minimalistyczny pasek statystyki (np. zdrowia) z mechaniką Ghost Bar (opóźniony spadek po ciosie).
+ */
 UCLASS(Abstract)
 class MYPROJECT_API UStatBarWidget : public UUserWidget
 {
 	GENERATED_BODY()
 
 public:
+	UStatBarWidget(const FObjectInitializer& ObjectInitializer);
+
 	// API publiczne dla kontrolera widoku (Aktualizacja stanu paska)
 	UFUNCTION(BlueprintCallable, Category = "UI|DataBinding")
 	void UpdateRatio(float CurrentValue, float MaxValue);
@@ -20,7 +25,21 @@ public:
 	void SetBarColor(FLinearColor NewColor);
 
 protected:
-	// Wiązanie komponentu wizualnego z Blueprintem (Odpowiednik @FXML lub bindingu w szablonie)
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+	// Główny pasek postępu (natychmiastowa reakcja)
 	UPROPERTY(meta = (BindWidget))
-	UProgressBar* StatProgressBar;
+	TObjectPtr<UProgressBar> StatProgressBar;
+
+	// Opcjonalny pasek widmo (Ghost Bar) - płynnie dogania główny pasek po otrzymaniu obrażeń
+	UPROPERTY(meta = (BindWidgetOptional))
+	TObjectPtr<UProgressBar> GhostProgressBar;
+
+	// Prędkość doganiania paska ghost (wyższa wartość = szybsze doganianie)
+	UPROPERTY(EditDefaultsOnly, Category = "UI|Config", meta = (ClampMin = "0.5", ClampMax = "10.0"))
+	float GhostInterpSpeed = 2.5f;
+
+private:
+	float TargetPercent = 1.0f;
+	float CurrentGhostPercent = 1.0f;
 };
